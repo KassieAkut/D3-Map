1cdcc452ca8ada8ac1beb36b596780fdec7c35e8
KassieAkut <82506170+KassieAkut@users.noreply.github.com>
2023-10-13 08:53:08 +0100

    app.js

--- D3/app.js
+++ D3/app.js
@@ -1,4 +1,5 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+import topojson from 'https://cdn.jsdelivr.net/npm/topojson@3.0.2/+esm'
 import { feature } from '.\topojson';
 
 const width = 900;
